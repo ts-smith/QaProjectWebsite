a44752fbfda9bc56ba9870de819a331b0c27badb
TrevorSSmith1392 <trevorssmith1392@gmail.com>
2013-07-16 13:47:11 -0700

    Labels on forms

--- modules.js
+++ modules.js
@@ -106,7 +106,7 @@ angular.module('html6', []).
     }).
     directive('countries', function(){
         return function(scope){
-            scope.countries = ["United States", "Afghanistan","Albania","Algeria","Andorra","Angola","Anguilla","Antigua; Barbuda","Argentina","Armenia","Aruba","Australia","Austria","Azerbaijan","Bahamas"
+            scope.countries = ["United States", "Other/Not Country", "Afghanistan","Albania","Algeria","Andorra","Angola","Anguilla","Antigua; Barbuda","Argentina","Armenia","Aruba","Australia","Austria","Azerbaijan","Bahamas"
                 ,"Bahrain","Bangladesh","Barbados","Belarus","Belgium","Belize","Benin","Bermuda","Bhutan","Bolivia","Bosnia; Herzegovina","Botswana","Brazil","British Virgin Islands"
                 ,"Brunei","Bulgaria","Burkina Faso","Burundi","Cambodia","Cameroon","Cape Verde","Cayman Islands","Chad","Chile","China","Colombia","Congo","Cook Islands","Costa Rica"
                 ,"Cote D Ivoire","Croatia","Cruise Ship","Cuba","Cyprus","Czech Republic","Denmark","Djibouti","Dominica","Dominican Republic","Ecuador","Egypt","El Salvador","Equatorial Guinea"
